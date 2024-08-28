576f2a6159c8f888ca45a126223ceaa33a3a4f75
Gabriella Victória <gabriellavictoria2819@gmail.com>
2024-08-28 15:07:45 -0300

    adicao dos nomes dos desenvolvedores do site no footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ function Footer() {
   };
 
   return (
-    <footer className="w-full bg-black text-white py-8 relative flex flex-row items-center justify-center">
+    <footer className="w-full bg-black text-white py-4 relative flex flex-row items-center justify-center">
+      <p className='font-montserrat text-xs left-0'>Gabriella Victória Cunha Melo dos Santos I Vinicus Gomes da Silva I Ana Clara Gomes da Silva I Pedro Bertolline Machado © 2024</p>
       <a 
         onClick={scrollToTop} 
         className="absolute right-10 flex items-center px-2 py-2 rounded-md bg-main-purple cursor-pointer"
